0527055799b643594c753f72fbe0811781e646e7
Dafik G <g.dafik15@gmail.com>
2024-06-15 08:36:02 +0530

    ec2-role-s3-bucket

---
+++ main.tf
@@ -0,0 +1,53 @@
+resource "aws_iam_role" "ec2-role" {
+    name = var.role_name
+  assume_role_policy = jsonencode(
+    {
+        version = "2012-10-17",
+        statement = [
+            {
+                Action : "sts.AssumeRole",
+                Effect : "Allow",
+                Principal = {
+                    service = "ec2.amazonaws.com"
+                }
+            }
+        ]
+    }
+  )
+  tags = {
+    Name = var.role_name
+    Environment = var.environment
+  }
+}
+
+# creating policy to attach to role with s3-read-only-access
+
+resource "aws_iam_policy" "policy_name" {
+  name = var.policy_name
+  policy = jsonencode(
+    {
+        version = "2012-10-17",
+        statement = [
+            {
+                Effect : "Allow",
+                Action :[
+                    "s3:GetObject",
+                    "s3:ListObject"
+                ]
+                Resource : "arn:aws:s3:::var.s3-bucket-name"
+            }
+        ]
+    }
+  )
+  tags = {
+    Name = var.policy_name
+    Environment = var.environment
+  }
+}
+
+# attaching policy to role
+
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  policy_arn = aws_iam_policy.policy_name.arn
+  role = aws_iam_role.ec2-role.name
+}
