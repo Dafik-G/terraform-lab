6f739288deeb3f9513254d135debe525cbd31f69
Dafik G <g.dafik15@gmail.com>
2024-06-15 12:51:29 +0530

    ec2-role-s3-bucket

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource "aws_iam_role" "ec2-role-s3" {
   name = var.role_name
   assume_role_policy = jsonencode(
     {
-      Version = "2012-10-17",
       Statement = [
         {
           Action : "sts.AssumeRole",
